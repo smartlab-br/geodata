f9d2510fea6ba68e8a0bdd12bef2b5388c81bea9
Rodrigo Fagundes <rodrigo.mfagundes@mpt.mp.br>
2020-05-07 12:18:50 -0300

    Multithreading

--- scripts/br/geo2topo.py
+++ scripts/br/geo2topo.py
@@ -3,35 +3,43 @@ import json
 import os
 import subprocess
 from numpy import linspace
+import datetime
+# import multiprocess
 
 def convert(origin, dest, quality_levels, skip_existing):
     # Skip if all levels have already been generated and the flag is set
     if skip_existing and all([os.path.isfile(dest.replace('q0', f'q{quality_key+1}')) for quality_key, quality_simpl in enumerate(quality_levels)]):
         return
+    
+    print(f"Reading from {origin}")
     with open(origin, 'r') as json_file:
         data = json.load(json_file)
         json_file.close()
-
+    print(f"Generating topology @ {datetime.datetime.now()}")
     tj_base = topojson.Topology(data.get('features'), presimplify = False, prequantize=False, topology=True)
+    print(f"Topology created successfully @ {datetime.datetime.now()}")
     for quality_key, quality_simpl in enumerate(quality_levels):
         dest_q = dest.replace('q0', f'q{quality_key+1}')
         # Skips if destination fila already exists and the flag is set
         if skip_existing and os.path.isfile(dest_q):
             continue
-        print(f"Generating from {origin} with simplification {quality_simpl} ({quality_key + 1})")
+        print(f"Generating topojson with simplification {quality_simpl} ({quality_key + 1})")
         tj = tj_base.toposimplify(quality_simpl)
         if not os.path.exists(os.path.dirname(dest_q)):
             os.makedirs(os.path.dirname(dest_q), exist_ok=True)
         with open(dest_q, "w") as topo_file:
-            json.dump(tj.to_json(), topo_file)
+            json.dump(tj.to_dict(), topo_file)
             topo_file.close()
+    print(f"Files generated from {origin} @ {datetime.datetime.now()}")
 
 skip_existing = True
 strt = "../../geojson"
 
+print(f"Starting... @ {datetime.datetime.now()}")
+
 quality_levels = list(linspace(0,0.01,4))
 quality_levels.reverse()
-print(quality_levels)
+print(f"Simplification levels: {quality_levels}")
 
 # for root, dirs, files in os.walk(strt):
 #     path = root.replace("geojson", "")
@@ -43,16 +51,21 @@ print(quality_levels)
 #                 convert(origin, dest, quality_levels, True)
 #             except:
 #                 continue
-#             # subprocess.call(['geo2json',origin,'>',dest])
-#             break
+
+## Multithread alternative
+with multiprocessing.Pool(processes=4) as pool:
+    for root, dirs, files in os.walk(strt):
+        path = root.replace("geojson", "")
+        pool.starmap(convert, ('../../geojson{}/{}'.format(path,file), '../../topojson{}/{}'.format(path,file), quality_levels, True))
+    # pool.close()
 
 ## Teste UF
-convert(
-    '../../geojson/br/uf/regic_saude_regioes_alta_complexidade/29_q0.json',
-    '../../topojson/br/uf/regic_saude_regioes_alta_complexidade/29_q0.json',
-    quality_levels,
-    False
-)
+# convert(
+#     '../../geojson/br/uf/regic_saude_regioes_alta_complexidade/29_q0.json',
+#     '../../topojson/br/uf/regic_saude_regioes_alta_complexidade/29_q0.json',
+#     quality_levels,
+#     False
+# )
 
 ## Teste BR
 # convert(
@@ -61,3 +74,5 @@ convert(
 #     quality_levels,
 #     False
 # )
+
+print(f"Done!!!! @ {datetime.datetime.now()}")
