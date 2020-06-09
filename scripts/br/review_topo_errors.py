b6bb2d56cad023fa1d79c7c66c1f9702baa0d79e
Rodrigo Fagundes <rodrigo.mfagundes@mpt.mp.br>
2020-06-09 07:25:46 -0300

    Adding topologies.

--- scripts/br/review_topo_errors.py
+++ scripts/br/review_topo_errors.py
@@ -7,15 +7,25 @@ intersections = []
 ring_self_intersections = []
 purged_geometries = []
 affected_file = None
-with open('log/log_topology.log', 'r') as log_file:
+with open('log/log_topology_old.log', 'r') as log_file:
     for line in log_file.readlines():
         if 'Generating topology from file' in line:
-            match = re.match(r'file (.*)', line)
+            match = re.search(r'file (.*)', line)
             affected_file = match.group(1)
+
+            affected_level = affected_file.replace('../../geojson/br/','').split('/')[0]
+            if '_q0.json' in affected_level:
+                affected_resolution = affected_level.replace('_q0.json','')
+                affected_level = 'br'
+            else:
+                affected_resolution = affected_file.replace('../../geojson/br/','').split('/')[1]
+            
         if 'found non-noded intersection between' in line:
-            match = re.match(r'LINESTRING \((-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?), (-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?)\) at (-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?)', line)
+            match = re.search(r'LINESTRING \((-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?), (-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?)\) and LINESTRING \((-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?), (-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?)\) at (-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?)', line)
             intersections.append({
                 'affected_file': affected_file,
+                'affected_level': affected_level,
+                'affected_resolution': affected_resolution,
                 'line_a_point_a_long': match.group(1),
                 'line_a_point_a_lat': match.group(3),
                 'line_a_point_b_long': match.group(5),
@@ -28,19 +38,28 @@ with open('log/log_topology.log', 'r') as log_file:
                 'intersect_lat': match.group(19)
             })
         if 'Self-intersection at or near point' in line:
-            match = re.match(r'Self-intersection at or near point (-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?)', line)
+            match = re.search(r'Self-intersection at or near point (-?[0-9]\d*\.(\d+)?) (-?[0-9]\d*\.(\d+)?)', line)
             ring_self_intersections.append({
                 'affected_file': affected_file,
+                'affected_level': affected_level,
+                'affected_resolution': affected_resolution,
                 'long': match.group(1),
                 'lat': match.group(3),
             })
-        if 'invalid geometric objects' in line:
-            match = re.match(r'removed (\d+) invalid geometric objects', line)
-            purged_geometries.append(
+        if 'invalid geometric object' in line:
+            match = re.search(r'removed (\d+) invalid geometric object', line)
+            purged_geometries.append({
                 'affected_file': affected_file,
+                'affected_level': affected_level,
+                'affected_resolution': affected_resolution,
                 'n_purged': match.group(1)
-            )
+            })
 
 df_intersections = pd.DataFrame(intersections)
+df_intersections.to_csv('log/analysis/errors/non_noded_instersections.csv', index=False)
+
 df_ring_self_intersections = pd.DataFrame(ring_self_intersections)
+df_ring_self_intersections.to_csv('log/analysis/errors/ring_self_instersections.csv', index=False)
+
 df_purged_geometries = pd.DataFrame(purged_geometries)
+df_purged_geometries.to_csv('log/analysis/errors/purged_geometries.csv', index=False)
